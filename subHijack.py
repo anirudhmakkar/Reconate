a18db96b39910019c448ee73df54ba449c0d29ed
root <anirudh.makkar@gmail.com>
2021-09-29 07:08:44 -0400

    subHijack added

---
+++ subHijack.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+slack_webhook_url = "https://hooks.slack.com/services/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"
+
+
+def slack_notifications(message):
+    slack_data = {'text': "Subdomain Takeover Possible: {}".format(message)}
+    response = requests.post(slack_webhook_url, data=json.dumps(slack_data))
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+
+filepath = 'subzyresults.txt'
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       result = line.find('VULNERABLE')
+       if result == 8:
+           slack_notifications(line)
+       line = fp.readline()
+       cnt += 1
+
+
+
+
